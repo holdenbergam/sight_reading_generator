c775704b7f082151fc499c9fb269035c7de24963
holdenbergam <109397505+holdenbergam@users.noreply.github.com>
2022-07-16 17:45:12 -0400

    initial commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,53 @@
+import os
+from flask import Flask, render_template, request, url_for, flash, redirect
+from .processing import make_random_music
+# ...
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+
+    @app.route('/result', methods=['GET', 'POST'])
+    def shortenurl():
+        if request.method == 'POST':
+            time = request.form['time']
+            key = request.form['key']
+            instrument = request.form['instrument']
+
+            #music = make_random_music(time, key, instrument)
+            #image = make_image(music)
+
+            return render_template('result.html', time=time, key=key, instrument=instrument)
+        elif request.method == 'GET':
+            return 'A GET request was made'
+        else:
+            return 'Not a valid request method for this route'
+
+    return app
---
+++ flaskr/processing.py
@@ -0,0 +1,17 @@
+import random
+####
+# create a list of notes
+def make_random_music(time_signature, key_signature, num_measures=4):
+    #return [[(34, 1/2), (37, 1/4), 33, ...], [(23, 1), ...] , ...]
+    c1 = 1, csharp1 = 2, d = 3, dsharp1 
+    quarter = 1
+    half = 2
+    whole = 4
+    keyoptions = ['sharp', 'flat', 'natural']
+
+def make_image(music):
+    return 0
+
+if __name__=='__main__':
+    print('hello world')
+    # make_random_music('2/4')
